e8e30d8d61fcbec55477e8a502553688b28c090e
Andrin <65789180+codeofandrin@users.noreply.github.com>
2024-12-30 00:58:12 +0100

    Revert "Add lenis smooth scroll"
    This reverts commit f24eb1f954fdf6d6e8fcc60aa29d36d73f605b08.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
-import SmoothScroll from "@/components/SmoothScroll"
 import "./globals.css"
 
 export default function RootLayout({
@@ -9,14 +8,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <SmoothScroll>
-      <html lang="en">
-        <body className="flex flex-col items-center">
-          <Navbar />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </SmoothScroll>
+    <html lang="en">
+      <body className="flex flex-col items-center">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
   )
 }

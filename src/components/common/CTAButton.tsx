51ba175de0aa186fb16b87474b9062736884a8cf
Andrin <65789180+codeofandrin@users.noreply.github.com>
2024-12-31 02:15:21 +0100

    Upscale and downscale cta button on hover and tap

--- src/components/common/CTAButton.tsx
+++ src/components/common/CTAButton.tsx
@@ -1,16 +1,23 @@
+"use client"
+
+import { motion } from "framer-motion"
+import type { HTMLMotionProps } from "framer-motion"
+
 import SVGArrowRight from "@/assets/icons/ArrowRight.svg"
 import "@/styles/root/CTAButton.css"
 
-interface CTAButtonPropsType
-  extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+interface CTAButtonPropsType extends Omit<HTMLMotionProps<"button">, "ref"> {
   children: string
 }
 
 export default function CTAButton({ children, ...props }: CTAButtonPropsType) {
   return (
-    <button
+    <motion.button
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.98 }}
+      transition={{ type: "spring", bounce: 0.6 }}
       {...props}
-      className={`${props.className} btn-cta group relative overflow-hidden rounded-full bg-gradient-to-t from-primary-600 to-primary-500 px-7 py-3.5 transition`}>
+      className={`${props.className} btn-cta group relative overflow-hidden rounded-full bg-gradient-to-t from-primary-600 to-primary-500 px-7 py-3.5`}>
       <div className="flex items-center justify-center">
         <p className="font-bold text-white">{children}</p>
         <SVGArrowRight className="ml-2 w-6 text-white transition-[transform] duration-300 group-hover:translate-x-2" />
@@ -18,6 +25,6 @@ export default function CTAButton({ children, ...props }: CTAButtonPropsType) {
       <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
         <div className="relative h-full w-8 bg-white/20"></div>
       </div>
-    </button>
+    </motion.button>
   )
 }

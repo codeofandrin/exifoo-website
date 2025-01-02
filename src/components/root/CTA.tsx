9cf11b19ed09dd4df8c2f2e0645b83c4534a5a9d
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-01-02 23:12:33 +0100

    Add desktop view for cta section

--- src/components/root/CTA.tsx
+++ src/components/root/CTA.tsx
@@ -7,30 +7,30 @@ interface EllipsePropsType {
 }
 
 function Ellipse({ className }: EllipsePropsType) {
-  return <div className={`${className} absolute -z-[99] rounded-full bg-primary-500 blur-[100px]`} />
+  return <div className={`${className} absolute -z-[99] rounded-full bg-primary-500 blur-[150px]`} />
 }
 
 export function Background() {
   return (
     <>
-      <Ellipse className="-right-40 -top-10 h-52 w-52" />
-      <Ellipse className="-bottom-10 -left-10 h-40 w-40" />
+      <Ellipse className="-right-40 -top-10 h-52 w-52 sm:-right-28 sm:-top-28 sm:h-60 sm:w-60 md:right-28 lg:right-60" />
+      <Ellipse className="-bottom-10 -left-10 h-40 w-40 sm:left-10 sm:h-52 sm:w-52" />
     </>
   )
 }
 
 export default function CTA() {
   return (
-    <div className="relative mt-20 overflow-hidden">
+    <div className="relative mt-20 w-full overflow-hidden">
       <div className="flex flex-col items-center justify-center px-7 py-20">
         {/* Text */}
-        <div id="cta-text" className="text-center font-logo text-4xl font-bold tracking-tight">
+        <div className="cta-text sm:cta-text-sm p-1 text-center font-logo text-4xl font-bold tracking-tight sm:text-6xl">
           <p>Ready?</p>
-          <p>Start organizing today!</p>
+          <p className="mt-1 sm:mt-3">Start organizing today!</p>
         </div>
         {/* Button */}
-        <div className="mt-16 w-full">
-          <CTAButton className="w-full">Download for free</CTAButton>
+        <div className="mt-16 flex w-full justify-center sm:mt-20">
+          <CTAButton className="w-full sm:w-72">Download for free</CTAButton>
         </div>
       </div>
       <Background />

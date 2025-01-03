c8044bf06f07c676158c2ee1d0faa2a02c96f06f
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-01-03 22:25:33 +0100

    Add mobile view for not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,17 +16,19 @@ export default function NotFound() {
   }
 
   return (
-    <div className="mt-10 flex w-full flex-col items-center justify-center">
-      <Image src={ImgNotFoundIllus} alt={ImgNotFoundIllus.src} className="w-96" />
-      <h1 className="mt-6 font-logo text-6xl font-semibold text-neutral-800">Page not found</h1>
-      <p className="mt-8 text-lg font-medium text-neutral-500">
+    <div className="mt-10 flex w-full max-w-screen-xl flex-col items-center justify-center px-7">
+      <Image src={ImgNotFoundIllus} alt={ImgNotFoundIllus.src} className="w-60 sm:w-96" />
+      <h1 className="mt-4 text-center font-logo text-4xl font-semibold text-neutral-800 sm:mt-6 sm:text-6xl">
+        Page not found
+      </h1>
+      <p className="mt-6 text-center font-medium text-neutral-500 sm:mt-8 sm:text-lg">
         Sorry, we couldn't find the page you're looking for.
       </p>
-      <div className="mt-14 flex">
-        <Button color="accent" className="w-40" onClick={handleContactSupport}>
+      <div className="mt-8 flex w-full flex-col justify-center sm:mt-14 sm:flex-row">
+        <Button color="accent" className="w-full sm:w-40" onClick={handleContactSupport}>
           Contact Support
         </Button>
-        <Button color="primary" className="ml-5 w-40" onClick={handleReturnHome}>
+        <Button color="primary" className="mt-3 w-full sm:ml-5 sm:mt-0 sm:w-40" onClick={handleReturnHome}>
           Return Home
         </Button>
       </div>

d4da7bccd4ed07974166b03184bebaae16ad7b43
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-09-08 11:33:54 +0200

    Add buy me a coffee button to hero section

--- src/components/root/Hero.tsx
+++ src/components/root/Hero.tsx
@@ -1,23 +1,37 @@
 import Image from "next/image"
 
 import CTAButton from "../common/CTAButton"
+import Button from "../common/Button"
+import { GeneralLinks } from "@/utils/constants"
 import ExternalLink from "../common/ExternalLink"
 import ImgAppPreview from "@/assets/images/app_preview_hero.png"
+import ImgBMCLogo from "@/assets/images/bmc_logo.png"
 
 export default function Hero() {
   return (
     <div className="mt-14 w-full px-7 sm:mt-44 sm:max-w-screen-xl">
       <div className="sm:flex">
         <div className="sm:w-1/2">
-          <div className="flex w-fit items-center rounded-full bg-primary-50 py-2 pl-2 pr-3 text-sm font-medium text-primary-700 shadow-sm ring-1 ring-inset ring-primary-700/10">
-            <div className="rounded-full bg-primary-500 px-2 py-0.5 text-white">NEW</div>
-            <div className="ml-3">
-              Free to use and{" "}
-              <ExternalLink href="https://github.com/codeofandrin/exifoo" color="underline">
-                open source
-              </ExternalLink>{" "}
-              🚀
+          <div className="w-fit lg:flex">
+            <div className="flex w-fit items-center rounded-full bg-primary-50 py-2 pl-2 pr-3 text-sm font-medium text-primary-700 shadow-sm ring-1 ring-inset ring-primary-700/10">
+              <div className="rounded-full bg-primary-500 px-2 py-0.5 text-white">NEW</div>
+              <div className="ml-3">
+                Free to use and{" "}
+                <ExternalLink href="https://github.com/codeofandrin/exifoo" color="underline">
+                  open source
+                </ExternalLink>{" "}
+                🚀
+              </div>
             </div>
+            <Button
+              href={GeneralLinks.buyMeACoffeeURL}
+              target="_blank"
+              className="mt-2 w-full rounded-full border-[#b39b03] bg-[#ffdd04] font-bold sm:w-48 lg:ml-2 lg:mt-0">
+              <div className="flex items-center">
+                <Image src={ImgBMCLogo} alt="BMC Logo" className="w-6 select-none" />
+                <p className="ml-1 text-xs text-neutral-900">Buy me a coffee</p>
+              </div>
+            </Button>
           </div>
           {/* Heading */}
           <div className="mt-12 flex flex-col">
